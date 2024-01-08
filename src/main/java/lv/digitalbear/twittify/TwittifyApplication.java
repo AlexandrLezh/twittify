39fcabb23a70a88e3c628cc6e6abc3fcc5fb052f
AlexandrLezh <sasha_l@inbox.lv>
2024-01-08 18:21:38 +0200

    Web controller Greeting

---
+++ src/main/java/lv/digitalbear/twittify/TwittifyApplication.java
@@ -0,0 +1,13 @@
+package lv.digitalbear.twittify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwittifyApplication.class, args);
+	}
+
+}
