419300575b36b383c7923c5452ac297077afd808
AlexandrLezh <sasha_l@inbox.lv>
2024-01-19 18:57:05 +0200

    Added admin panel, bug fix for filter in main.ftl.

--- src/main/java/lv/digitalbear/twittify/domen/Role.java
+++ src/main/java/lv/digitalbear/twittify/domen/Role.java
@@ -3,7 +3,7 @@ package lv.digitalbear.twittify.domen;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-	USER;
+	USER, ADMIN;
 
 	@Override
 	public String getAuthority() {
