28041b965a5abb324fb047d348e120d38a391b78
AlexandrLezh <sasha_l@inbox.lv>
2024-01-26 17:03:38 +0200

    Validation ControllerUtils added

--- src/main/java/lv/digitalbear/twittify/domen/Message.java
+++ src/main/java/lv/digitalbear/twittify/domen/Message.java
@@ -1,6 +1,8 @@
 package lv.digitalbear.twittify.domen;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +15,8 @@ public class Message {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@NotBlank(message = "Fill message please.")
+	@Size(max = 2000, message = "Message cannot be more than 2000 characters.")
 	private String text;
 	private String tag;
 	@ManyToOne(fetch = FetchType.EAGER)
