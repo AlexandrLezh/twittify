7fd1cc2696d76968e15ceb9c613073a1879fd8cf
AlexandrLezh <sasha_l@inbox.lv>
2024-01-27 13:32:45 +0200

    Edit Message added

--- src/main/java/lv/digitalbear/twittify/domen/User.java
+++ src/main/java/lv/digitalbear/twittify/domen/User.java
@@ -3,46 +3,66 @@ package lv.digitalbear.twittify.domen;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "usr")
 public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	@NotBlank(message = "Username cannot be blank.")
+	@NotBlank(message = "Username cannot be empty")
 	private String username;
-	@NotBlank(message = "Password cannot be empty.")
+	@NotBlank(message = "Password cannot be empty")
 	private String password;
 	private boolean active;
-	@Email
-	@NotBlank(message = "Email cannot be empty.")
+	private static final long serialVersionUID = 6370841558618424225L;
+	@Email(message = "Email is not correct")
+	@NotBlank(message = "Email cannot be empty")
 	private String email;
 	private String activationCode;
 
 	@ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
-	@CollectionTable(name = "user_role",joinColumns = @JoinColumn(name = "user_id"))
+	@CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
 	@Enumerated(EnumType.STRING)
 	private Set<Role> roles;
+
+	@OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<Message> messages;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return Objects.equals(id, user.id);
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(id);
+	}
 
 	public boolean isAdmin() {
 		return roles.contains(Role.ADMIN);
 	}
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return getRoles();
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
 	}
 
 	@Override
@@ -64,4 +84,62 @@ public class User implements UserDetails {
 	public boolean isEnabled() {
 		return isActive();
 	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return getRoles();
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getActivationCode() {
+		return activationCode;
+	}
+
+	public void setActivationCode(String activationCode) {
+		this.activationCode = activationCode;
+	}
+
+	public Set<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(Set<Message> messages) {
+		this.messages = messages;
+	}
+
 }
--- src/main/java/lv/digitalbear/twittify/repos/MessageRepo.java
+++ src/main/java/lv/digitalbear/twittify/repos/MessageRepo.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface MessageRepo extends JpaRepository<Message, Long> {
 
 	List<Message> findByTag(String tag);
+
 }
